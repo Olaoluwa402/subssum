f42b98bf382db5e86270a10e59c3aa44e0e1d67c
Olaoluwa <ibukundaniel402@yahoo.co.uk>
2024-08-31 16:55:29 +0100

    fix: shago factory

--- src/modules/api/bill/index.ts
+++ src/modules/api/bill/index.ts
@@ -1,11 +1,20 @@
-import { Module } from "@nestjs/common";
+import { Module, Provider } from "@nestjs/common";
 import { AirtimeBillController } from "./controllers/v1/airtime";
 import { AirtimeBillService } from "./services/airtime";
+import { Shago } from "@/libs/shago";
+import { shagoOptions } from "@/config";
+
+export const ShagoProvider: Provider = {
+    provide: "SHAGO",
+    useFactory: () => {
+        const shago = new Shago(shagoOptions);
+        return shago;
+    },
+};
 
 @Module({
     imports: [],
-    providers: [AirtimeBillService],
     controllers: [AirtimeBillController],
-    exports: [],
+    providers: [AirtimeBillService, ShagoProvider],
 })
 export class BillModule {}
--- src/modules/api/index.ts
+++ src/modules/api/index.ts
@@ -19,3 +19,4 @@ import { WebExtension } from "./webExtension";
     ],
 })
 export class APIModule {}
+ 
